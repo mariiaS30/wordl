a860dfa108ee6b28a081dd49ead08adeb1314879
Mariia S <thimariia@gmail.com>
2023-09-04 22:05:36 -0700

    написали алгоритм разгадывания слов

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 words = {
     'подстаканник': 'подставка под посуду',
     'подушка' : 'то на чём мы спим',
@@ -6,5 +8,27 @@ words = {
     'зеркало':'ты можешь себя увидеть'
 }
 
-print(words['сумка'])
-#ключи в список. рандом.чойс для этого списка.
+words_list = list(words)
+
+word = random.choice(words_list)
+explanation = words[word]
+game_word = '*' * len(word)
+
+print(explanation)
+print(game_word)
+
+while game_word.find('*') != -1:
+    print('ВВедите букву')
+    letter =  input()
+    if word.find(letter) != -1:
+        game_word = list(game_word)
+        for i in range(0, len(word)):
+            if word[i] == letter:
+                game_word[i] = letter
+        game_word = ''.join(game_word)
+    print(game_word)
+
+# game_word = list(game_word)
+# game_word[0] = 'k'
+# game_word = ''.join(game_word)
+# print(game_word)

c9a0ad9ea4677e2f22be1d2c55156a2acba56fb1
Mariia S <thimariia@gmail.com>
2023-09-13 22:03:44 -0700

    написали программу с возможность повторов слов

--- main.py
+++ main.py
@@ -5,30 +5,50 @@ words = {
     'подушка' : 'то на чём мы спим',
     'бутылка':'что-то жидкое внутри',
     'сумка':'женщины и мужчины носят это',
-    'зеркало':'ты можешь себя увидеть'
+    'зеркало':'ты можешь себя увидеть',
+    'компьютер':'то на чём ты можешь работать',
+    'телефон':'звонки',
+    'дерево':'зелёное сверху, коричневое снизу',
+    'лампа':'висит груша, нельзя скушать',
+    'ананас':'какой ингредиент содержит своеобразная пицца'
 }
-
+continue_game = 'Y'
 words_list = list(words)
 
-word = random.choice(words_list)
-explanation = words[word]
-game_word = '*' * len(word)
+while continue_game == 'Y':
+    word = random.choice(words_list)
+    explanation = words[word]
+    game_word = '*' * len(word)
 
-print(explanation)
-print(game_word)
-
-while game_word.find('*') != -1:
-    print('ВВедите букву')
-    letter =  input()
-    if word.find(letter) != -1:
-        game_word = list(game_word)
-        for i in range(0, len(word)):
-            if word[i] == letter:
-                game_word[i] = letter
-        game_word = ''.join(game_word)
+    print(explanation)
     print(game_word)
 
-# game_word = list(game_word)
-# game_word[0] = 'k'
-# game_word = ''.join(game_word)
-# print(game_word)
+    while game_word.find('*') != -1:
+        print('Введите букву или слово')
+        letter =  input()
+        if len(letter) == 1:
+            if word.find(letter) != -1:
+                game_word = list(game_word)
+                for i in range(0, len(word)):
+                    if word[i] == letter:
+                        game_word[i] = letter
+                game_word = ''.join(game_word)
+            else:
+                print('Введите другую букву')
+            print(game_word)
+        else:
+            if letter == word:
+                game_word = word
+                print(word)
+                print('Ответ верный!')
+            else:
+                print('Ответ неверный!')
+
+    print('Хотите сыграть ещё? Y/N?')
+    continue_game = input() 
+    
+print('Игра окнчена')
+
+
+# слова не должны повторяться 
+
